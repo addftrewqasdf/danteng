d89c5ce7f5c2f7b1cab0913a7952814335f05f5f
goldfish <27198928@qq.com>
2012-11-05 19:38:14 +0800

    first

---
+++ 123123123/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  123123123
+//
+//  Created by Ibokan on 12-11-5.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
